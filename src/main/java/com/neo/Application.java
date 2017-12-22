6980153514a9235b42ad296fd280cc0544b21a4a
wangwei <752340929@qq.com>
2017-12-22 12:52:49 +0800

    整理代码

--- src/main/java/com/neo/Application.java
+++ src/main/java/com/neo/Application.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+/**
+ * 工程启动类
+ * @author Administrator
+ *
+ */
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 	
